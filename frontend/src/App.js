46aa84b03ce23f839183504191b48bb6f35d9891
Nikhil Singh <nikhilbangalor@gmail.com>
2022-03-03 18:38:42 +0530

    day 0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import shopItLogo from "./assets/Shop_it_Logo.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={shopItLogo} className="App-logo" alt="logo" />
+        <p>Coming Soon.</p>
       </header>
     </div>
   );
