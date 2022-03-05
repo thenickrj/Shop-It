927925da0b76644efee960c575bbf588d9996f90
Nikhil Singh <nikhilbangalor@gmail.com>
2022-03-05 23:37:35 +0530

    homepage day 3

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,26 +1,36 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import Product from "../components/Product";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productActions";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 
 function HomePage() {
-  let [products, setProducts] = useState();
+  let dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  console.log(productList);
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    fetch("/api/products")
-      .then((response) => response.json())
-      .then((data) => {
-        // console.log(data.json());
-        setProducts(data);
-      });
-  });
+    dispatch(listProducts());
+  }, []);
 
   return (
-    <div>
-      HomePage
-      <div className="row center">
-        {products.map((product) => (
-          <div>{product.name}</div>
-        ))}
-      </div>
+    <div class="page_content">
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <>
+          {products.length === 0 && <MessageBox>No Product Found</MessageBox>}
+          <div className="row center">
+            {products.map((product) => (
+              <Product key={product._id} product={product} />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
