9d8fc24f02c3e4ed5d9483fdb1734c44190aef30
Nikhil Singh <nikhilbangalor@gmail.com>
2022-03-05 23:37:56 +0530

    day 3

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
 
-function Products({product}) {
+function Product({ product }) {
   return (
-    <div key={product._id}>Products</div>
-  )
+    <div className="product_container" key={product._id}>
+      <Link to={`/product/${product._id}`}>
+        <img className="medium" src={product.image} alt="product" />
+      </Link>
+      <div className="card_body">
+        <Link to="product.html">
+          <h2>{product.name}</h2>
+        </Link>
+        <Rating rating={product.rating} numReviews={product.numReviews} />
+        <div className="row">
+          <div className="price">₹ {product.price}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Products
+export default Product;
