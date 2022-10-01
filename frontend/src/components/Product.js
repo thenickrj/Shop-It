2198b2db84627d2cce5d401b03ed493bce4ff31a
Nikhil Singh <nikhilbangalor@gmail.com>
2022-10-01 14:58:20 +0530

    1 Oct

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,23 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
+import styled from "styled-components";
+
+const Container = styled.div`
+  a {
+    color: #212121;
+  }
+  a:visited {
+    color: #3569a2;
+  }
+`;
 
 function Product({ product }) {
   return (
-    <div className="product_container" key={product._id}>
-      <Link to={`/product/${product._id}`}>
-        <img className="medium" src={product.image} alt="product" />
-      </Link>
-      <div className="card_body">
+    <Container>
+      <div className="product_container" key={product._id}>
         <Link to={`/product/${product._id}`}>
-          <h2>{product.name}</h2>
+          <img className="medium" src={product.image} alt="product" />
         </Link>
-        <Rating rating={product.rating} numReviews={product.numReviews} />
-        <div className="row">
-          <div className="price">₹ {product.price}</div>
+        <div className="card_body">
+          <Link to={`/product/${product._id}`}>
+            <h2>{product.name}</h2>
+          </Link>
+          <Rating rating={product.rating} numReviews={product.numReviews} />
+          <div className="row">
+            <div className="price">₹ {product.price}</div>
+          </div>
         </div>
       </div>
-    </div>
+    </Container>
   );
 }
 
